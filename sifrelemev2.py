7068a3f1674b0220b048fb57a640ceb8e76fe94e
zarganax <54045618+zarganax@users.noreply.github.com>
2019-08-13 04:15:22 -0400

    Create sifrelemev2.py

---
+++ sifrelemev2.py
@@ -0,0 +1,28 @@
+print("Programa hoşgeldin.")
+sifre=input("Lütfen parolayı giriniz.\n")
+sifre=int(sifre)
+__import__("os").system("clear")
+
+if sifre==123: 
+	print("Parolayı doğru girdiniz.")
+	input("Bilgilere erişmek için herhangi bir tuşa basınız.\n")
+	__import__("os").system("clear")
+	print("ILHAN_AKIN isimli modemin şifresi: ilhan2005")
+
+else:
+	print("Şifreyi yanlış girdiniz.")
+input("Yeniden denemek isterseniz bir tuşa basınız.Kalan deneme hakkı:1\n")
+__import__("os").system("clear")
+
+sifre=input("Lütfen parolayı giriniz.\n")
+sifre=int(sifre)
+__import__("os").system("clear")
+
+if sifre==123: 
+	print("Parolayı doğru girdiniz.")
+	input("Bilgilere erişmek için herhangi bir tuşa basınız.\n")
+	__import__("os").system("clear")
+	print("ILHAN_AKIN isimli modemin şifresi: ilhan2005")
+
+else:
+	print("Şifreyi yanlış girdiniz.")
